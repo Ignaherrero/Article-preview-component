c44f33fb98aeecab5ba27f85837d0eb79c7f9c9b
Nacho <nachoherrero93@hotmail.com>
2020-08-12 16:56:50 -0300

    Fix index.html and scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
 const button = document.getElementById('button');
 const divpopuptext = document.getElementById('divpopuptext');
 button.addEventListener('click', () => {
-  console.log(button.classList.contains('active'));
   if (divpopuptext.classList.contains('active')) {
     divpopuptext.classList.remove('active');
   } else {
